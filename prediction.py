d414baf5567a9ee770dfb19ff8f9b939b738ab03
Folsiti <folsiti@163.com>
2024-09-11 22:50:37 +0800

    Empty commit

--- prediction.py
+++ prediction.py
@@ -1,7 +1,9 @@
 import argparse
 import os
 import pandas as pd
+
 from bert_sklearn import load_model
+
 
 def flatten(l):
     return [item for sublist in l for item in sublist]
@@ -19,38 +21,69 @@ def read_data(filename, idx=0):
     lines = [line.split("\n") for line in lines]
 
     # get tokens
-    tokens = [[l.split()[0] for l in line] for line in lines]
+    tokens = [[l[0] for l in line] for line in lines]
 
     # get labels/tags
     labels = [[l.split()[idx] for l in line] for line in lines]
 
     #convert to df
     data = {'tokens': tokens, 'labels': labels}
+    #data = {'tokens': tokens}
     df = pd.DataFrame(data=data)
     return df
 
 def get_data(load_data):
-    data = read_data(load_data, idx=0)
-    print("Train and dev data: %d sentences, %d tokens" % (len(data), len(flatten(data.tokens))))
+    data = read_data(load_data, idx=1)
+    print("Prediction data: %d sentences, %d tokens" % (len(data), len(flatten(data.tokens))))
     print(data)
-    return data.tokens, data.labels
+    return data
 
 def save(path, tokens, labels):
-    save_data = {'tokens': tokens, 'labels': labels}
+    out_tokens = list()
+    out_labels = list()
+    for i in tokens:
+        p = ""
+        for s in i:
+            p += s
+        out_tokens.append(p)
+    for i in labels:
+        out_labels.append(max(i, key=i.count))
+    save_data = {'tokens': out_tokens, 'labels': out_labels}
     df = pd.DataFrame(data=save_data)
-    save_csv = path + 'out.csv'
-    df.to_csv(save_csv)
+    out_csv = path + 'new_out.csv'
+    df.to_csv(out_csv)
 
 def main(opt):
-    tokens, labels = get_data(opt.data)
+    data = get_data(opt.data)
+    tokens = data.tokens
     model = load_model(opt.model)
     pred_label = model.predict(tokens)
-    save(opt.outputpath, tokens, pred_label)
+    # pred = sklearn.preprocessing.MultiLabelBinarizer().fit_transform(pred_label)
+    # y_test = sklearn.preprocessing.MultiLabelBinarizer().fit_transform(data.labels)
+
+    tp=tn=fn=fp=0
+
+    for i,j in zip(pred_label,data.labels):
+        pr=max(i, key=i.count)
+        tr=max(j, key=j.count)
+        if pr==tr:
+            if tr=='Circle':
+                tp+=1
+            else:
+                tn+=1
+        else:
+            if tr=='Circle':
+                fp+=1
+            else:
+                fn+=1
+
+    #print(classification_report(flatten(labels), flatten(pred_label)))
+    save(opt.outputpath, tokens,pred_label)
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data', type=str, default='DATADIR/c_test.txt', help='initial datapath path')
-    parser.add_argument('--model', type=str, default='save/out.bin', help='initial model path')
+    parser.add_argument('--data', type=str, default='DATADIR/test.txt', help='initial datapath path')
+    parser.add_argument('--model', type=str, default='save/out.bin', help='initial model path') #cyc_out.bin
     parser.add_argument('--outputpath', type=str, default='DATADIR/', help='output path')
     opt = parser.parse_args()
     if os.path.exists(opt.outputpath) == False:
--- train.py
+++ train.py
@@ -1,12 +1,14 @@
 import os
 import csv
+import sys
 import argparse
 
 import numpy as np
 import pandas as pd
 from sklearn.metrics import classification_report
-from bert_sklearn import BertTokenClassifier
 
+sys.path.append("../")
+from bert_sklearn import BertTokenClassifier
 
 def read_tsv(filename, quotechar=None):
     with open(filename, "r", encoding='utf-8') as f:
@@ -25,7 +27,6 @@ def read_txt(filename, idx=1):
      # split on newlines
     lines = [line.split("\n") for line in lines]
 
-    # get tokens
     tokens = [[l.split()[0] for l in line] for line in lines]
 
     # get labels/tags
@@ -37,15 +38,21 @@ def read_txt(filename, idx=1):
     return df
 
 def get_data(opt):
+    '''
+    trainfile = DATADIR + "train.txt",
+    devfile = DATADIR + "dev.txt",
+    testfile = DATADIR + "test.txt"
+    '''
     trainfile = opt.train_data
     id = opt.labels_idx
     train = read_txt(trainfile, idx=id)
-    print("Train and dev data: %d sentences, %d tokens" % (len(train), len(flatten(train.tokens))))
+    print("Train data: %d sentences, %d tokens" % (len(train), len(flatten(train.tokens))))
     #print(train)
 
     testfile = opt.test_data
     test = read_txt(testfile, idx=id)
     print("Test data: %d sentences, %d tokens"%(len(test),len(flatten(test.tokens))))
+    #train = pd.concat([train, test])
     #print(test)
 
     #return train, test
@@ -65,49 +72,64 @@ def main(opt):
 
     train.head()
 
-
-    model = BertTokenClassifier('bert-base-cased',
+    # bert-base-uncased
+    model = BertTokenClassifier(#'bert-base-cased',
                             max_seq_length=178,
-                            epochs=4,
-                            gradient_accumulation_steps=4,
-                            learning_rate=3e-5,
-                            train_batch_size=20,
-                            eval_batch_size=20,
-                            validation_fraction=0,
+                            epochs=16,
+                            gradient_accumulation_steps=1,
+                            learning_rate=5e-5,
+                            train_batch_size=16,
+                            eval_batch_size=16,
+                            validation_fraction=0.15,
                             label_list=label_list,
+                            ignore_label=['non_Circle'],
                             use_cuda=True)
 
     print(model)
     # finetune model on train data
-
+    #print(savepath + str(3) + '_cyc_out.bin')
     model.fit(X_train, y_train)
 
-
     # score model on test data
+
     # f1_test = model.score(X_test, y_test,'macro')
     # print("Test f1: %0.02f"%(f1_test))
 
     # get predictions on test data
     y_preds = model.predict(X_test)
 
-    save_data = {'tokens': X_test, 'labels': y_test, 'preds': y_preds}
+    #save test result
+    out_tokens = list()
+    out_pred_labels = list()
+    out_test_labels = list()
+    for i in X_test:
+        p = ""
+        for s in i:
+            p += s
+        out_tokens.append(p)
+    for i in y_preds:
+        out_pred_labels.append(max(i, key=i.count))
+    for i in y_test:
+        out_test_labels.append(max(i, key=i.count))
+    save_data = {'tokens': out_tokens, 'labels': out_test_labels,'preds': out_pred_labels}
     dfs = pd.DataFrame(data=save_data)
-    pred_save = opt.save_path + 'out.csv'
+    pred_save = opt.save_path + 'tset_out.csv'
     dfs.to_csv(pred_save)
 
     # print report on classifier stats
     print(classification_report(flatten(y_test), flatten(y_preds)))
-    model.save(savepath+'out.bin')
-    print("Successfully saved in "+savepath)
+    path = savepath+'non_pred_out.bin'
+    model.save(path)
+    print("Successfully saved in "+path)
 
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--train_data', type=str, default='DATADIR/x_train.txt', help='initial train database path')
-    parser.add_argument('--test_data', type=str, default='DATADIR/x_test.txt', help='initial test database path')
+    parser.add_argument('--train_data', type=str, default='DATADIR/train.txt', help='initial train database path')
+    parser.add_argument('--test_data', type=str, default='DATADIR/test.txt', help='initial test database path')
     parser.add_argument('--labels_idx', type=str, default=1, help='initial labels idx')
     parser.add_argument('--save_path', type=str, default='save/', help='save model path')
     opt = parser.parse_args()
     if os.path.exists(opt.save_path) == False:
-        os.makedirs(opt.save_path)
+        os.makedirs(opt.outputpath)
     main(opt)
